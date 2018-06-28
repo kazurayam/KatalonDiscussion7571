d47f5a66af38fe80c491405b0dcc93360b8d01ad
kazurayam <kazuaki.urayama@gmail.com>
2018-06-29 06:52:36 +0900

    check HTTP Response Status

--- Scripts/TC1/Script1530001886160.groovy
+++ Scripts/TC1/Script1530001886160.groovy
@@ -14,17 +14,28 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
 String url = "http://files.shareholder.com/downloads/AAPL/6323171818x0xS320193-17-70/320193/filing.pdf"
 
+HttpClient httpclient = HttpClients.custom().build()
+
+// or in case you are behind companies Proxy server
+/*
 HttpHost proxy = new HttpHost('172.24.2.10', 8080);
 RequestConfig config = RequestConfig.custom().setProxy(proxy).build();
 HttpClient httpclient = HttpClients.custom().setDefaultRequestConfig(config).build();
+ */
 
 HttpHead headRequest = new HttpHead(url);
 headRequest.setHeader("Content-Type", "application/pdf");
 
+// make a request and get a response
 HttpResponse response = httpclient.execute(headRequest);
 
+// check the response status
+def statusCode = response.getStatusLine().getStatusCode()
+
+// check a Content-Type header
 Header contentType = response.getFirstHeader("Content-Type");
 String value = contentType.getValue()
 
-WebUI.comment("contentType=${value}")
+// show the check result
+WebUI.comment(">>> statudCode=${statusCode} contentType=${value}")
 
